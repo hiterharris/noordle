09223af999bd5435966be07506121fd9ef1e6bb4
Hiter Harris <hiterharris@comcast.net>
2022-05-24 00:42:38 -0400

    set up first guess

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [word, setWord] = useState('NOORI');
+  const [input, setInput] = useState('');
+  const [guess, setGuess] = useState(input);
+  const [correct, setCorrect] = useState('');
+
+  const handleInput = e => setInput(e.target.value);
+
+  useEffect(() => {
+    setGuess(input.toUpperCase());
+  }, [input, correct]); 
+
+  const checkWord = () => {
+    setGuess(input);
+    console.log('word: ', word.toUpperCase());
+    console.log('guess: ', guess.toUpperCase());
+    guess == word ? setCorrect('correct') : setCorrect('incorrect');
+  }
+
+  useEffect(() => {
+    console.log(correct);
+  }, [correct]);  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wordle</h1>
+      {/* <p>{word}</p> */}
+      <div className="guess-input">
+      <input type="text" maxlength="5" value={input.toUpperCase()} onChange={e => handleInput(e)}></input>
+      <button type="submit" onClick={() => checkWord()}>Submit</button>
+      </div>
+       <table>
+         <tbody>
+          <tr className={`guess one ${correct}`}>
+            <td>{input[0]?.toUpperCase()}</td>
+            <td>{input[1]?.toUpperCase()}</td>
+            <td>{input[2]?.toUpperCase()}</td>
+            <td>{input[3]?.toUpperCase()}</td>
+            <td>{input[4]?.toUpperCase()}</td>
+          </tr>
+          <tr className="guess two">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr className="guess three">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr className="guess four">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr className="guess five">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
