6285c4805d35bc031aaec4ba3bf7fc8ac4f8126e
Hiter Harris <hiterharris@comcast.net>
2022-05-31 15:51:56 -0400

    check letter and position

--- src/App.js
+++ src/App.js
@@ -32,39 +32,53 @@ function App() {
     if (guess[0] === word[0]) {
       guessRound.results.push('green');
       guessRound.word = guess;
-    } else if (guess[0] !== word[0]) {
+    } else if (guess[0] !== word[0] && word.includes(guess[0])) {
+      guessRound.results.push('yellow');
+      guessRound.word = guess;
+    } else if (guess[0] !== word[0] && !word.includes(guess[0])) {
       guessRound.results.push('red');
       guessRound.word = guess;
     }
     if (guess[1] === word[1]) {
       guessRound.results.push('green');
       guessRound.word = guess;
-    } else if (guess[1] !== word[1]) {
+    } else if (guess[1] !== word[1] && word.includes(guess[1])) {
+      guessRound.results.push('yellow');
+      guessRound.word = guess;
+    } else if (guess[1] !== word[1] && !word.includes(guess[1])) {
       guessRound.results.push('red');
       guessRound.word = guess;
     }
     if (guess[2] === word[2]) {
       guessRound.results.push('green');
       guessRound.word = guess;
-    } else if (guess[2] !== word[2]) {
+    } else if (guess[2] !== word[2] && word.includes(guess[2])) {
+      guessRound.results.push('yellow');
+      guessRound.word = guess;
+    } else if (guess[2] !== word[2] && !word.includes(guess[2])) {
       guessRound.results.push('red');
       guessRound.word = guess;
     }
     if (guess[3] === word[3]) {
       guessRound.results.push('green');
       guessRound.word = guess;
-    } else if (guess[3] !== word[3]) {
+    } else if (guess[3] !== word[3] && word.includes(guess[3])) {
+      guessRound.results.push('yellow');
+      guessRound.word = guess;
+    } else if (guess[3] !== word[3] && !word.includes(guess[3])) {
       guessRound.results.push('red');
       guessRound.word = guess;
     }
     if (guess[4] === word[4]) {
       guessRound.results.push('green');
       guessRound.word = guess;
-    } else if (guess[4] !== word[4]) {
+    } else if (guess[4] !== word[4] && word.includes(guess[4])) {
+      guessRound.results.push('yellow');
+      guessRound.word = guess;
+    } else if (guess[4] !== word[4] && !word.includes(guess[4])) {
       guessRound.results.push('red');
       guessRound.word = guess;
     }
-    
   }
 
   const handleGuess = () => {
